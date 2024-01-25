63ccc538e93a956b7353b075195f39df35a5ffe2
Isaac Williams <isaac@isaacwilliams.net>
2024-01-25 21:28:10 +1100

    Avoid conditional hook

--- src/components/generators/customItem/useLocalStorage.js
+++ src/components/generators/customItem/useLocalStorage.js
@@ -1,13 +1,11 @@
 import { useState } from 'react';
 
 function useLocalStorage(key, initialValue) {
-    if (typeof window === 'undefined') {
-        return [initialValue, () => {}];
-    }
-
     // State to store our value
     // Pass initial state function to useState so logic is only executed once
     const [storedValue, setStoredValue] = useState(() => {
+        if (typeof window === 'undefined') return initialValue;
+
         try {
             // Get from local storage by key
             const item = window.localStorage.getItem(key);
@@ -22,6 +20,8 @@ function useLocalStorage(key, initialValue) {
     // Return a wrapped version of useState's setter function that ...
     // ... persists the new value to localStorage.
     const setValue = value => {
+        if (typeof window === 'undefined') return;
+
         try {
             // Allow value to be a function so we have same API as useState
             const valueToStore =
