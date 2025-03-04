2f47de67c216175aa392b2c5e11703dae726ace9
isaacwilliams <isaacwilliams@users.noreply.github.com>
2025-03-04 03:19:34 +0000

    Deploying to gh-pages from @ isaacwilliams/mausritter-web@687c61c1c91011ca88ce58e00647b4fe547f9bd1 🚀

---
+++ assets/chunks/chunk-CLNMdr6k.js
@@ -0,0 +1,7 @@
+import{d as t,f as e,m as o}from"./chunk-CWwA7P3U.js";const n=t.h2.withConfig({displayName:"shared__Title",componentId:"sc-1bgudxk-0"})([""," padding-bottom:2rem;font-size:2.5rem;text-align:center;",""],e.display,o.phone`
+        font-size: 2rem;
+    `),s=t.h3.withConfig({displayName:"shared__SubTitle",componentId:"sc-1bgudxk-1"})([""," padding-bottom:1rem;font-size:2rem;",""],e.display,o.phone`
+        font-size: 1.4rem;
+    `),a=t.div.withConfig({displayName:"BodyText",componentId:"sc-oarrbx-0"})(["font-family:interstate-condensed,sans-serif;font-size:1.4rem;line-height:1.4;strong{font-weight:bold;}h1,h2,h3,h4{",";}h1{font-size:2rem;}h2{font-size:1.4rem;}h3{font-size:1.2rem;}p,ul{margin-top:0.5em;margin-bottom:0.5em;}table{width:auto;}table{width:100%;border-collapse:collapse;}th,td{padding:0.5em;}th{background-color:#f0f0f0;font-weight:bold;font-size:16px;text-align:left;",";}tr:nth-child(even){background-color:#f9f9f9;}ul{list-style:disc;margin-left:1em;}ol{list-style:roman;margin-left:1em;}"," &.center{text-align:center;}&.small{font-size:1rem;}&.large{font-size:1.6rem;}"],e.display,e.display,o.phone`
+        font-size: 1.3rem;
+    `);export{a as B,s as S,n as T};
---
+++ assets/chunks/chunk-Czs3EjMm.js
@@ -0,0 +1,7 @@
+import{t as a,v as s,d as t,m as l,f as i,s as m}from"./chunk-CWwA7P3U.js";import{g}from"./chunk-Fzw1n6aE.js";function c(){let e=g();return e?o=>r(o,e):(e=a(),o=>{"_headAlreadySet"in e?p(o):r(o,e)})}function r(e,o){o._configFromHook??(o._configFromHook={}),Object.assign(o._configFromHook,e)}function p(e){const{title:o,lang:n}=e;s(o,n)}function u(e){return c()(e),null}const y=t.h1.withConfig({displayName:"generatorComponents__Title",componentId:"sc-lyjqol-0"})(["font-size:2.2rem;font-weight:bold;",";"],i.display),C=t.div.withConfig({displayName:"generatorComponents__TitleWrapper",componentId:"sc-lyjqol-1"})(["display:flex;align-items:center;justify-content:space-between;margin-top:5rem;margin-bottom:3rem;",""],l.phone`
+        display: block;
+        margin-top: 1rem;
+        margin-bottom: 2rem;
+        margin-left: 1rem;
+        margin-right: 1rem;
+    `),_=t.button.withConfig({displayName:"generatorComponents__RollButton",componentId:"sc-lyjqol-2"})(["padding:0.6rem 1.5rem;border:0;border-radius:0.2rem;background:#ddd;color:",";font-size:1.1rem;"," letter-spacing:0.05rem;cursor:pointer;outline:none;&:hover{background:yellow;color:black;}"],m.body,i.display);let h=(e=21)=>crypto.getRandomValues(new Uint8Array(e)).reduce((o,n)=>(n&=63,n<36?o+=n.toString(36):n<62?o+=(n-26).toString(36).toUpperCase():n>62?o+="-":o+="_",o),"");export{u as C,_ as R,C as T,y as a,h as n};
