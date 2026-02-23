7b79d6442cf317447f2fb2e8feb7f6831d250bb5
Nikita Chursin <chursinn91@gmail.com>
2026-02-23 18:21:52 +0300

    fixed overpass font for Russian

--- src/components/styles/font.js
+++ src/components/styles/font.js
@@ -2,7 +2,7 @@ import { css } from 'styled-components';
 
 export default {
     body: css`
-        font-family: 'interstate-condensed', 'Overpass Cyrillic', sans-serif;
+        font-family: 'interstate-condensed', 'Overpass', sans-serif;
     `,
     display: css`
         font-family: 'ff-brokenscript-bc-web', 'Moyenage Cyrillic', serif;
